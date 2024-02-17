cd9229600174abd1191a532879b705958cb7e176
mehrdad Osouli Saran <mehrdadosoulisaran@gmail.com>
2024-02-17 21:16:04 +0330

    test api request to front and get response in clientand added books files in backend and import to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
+import { books } from './constants/mockData.js'
 const express=require('express')
+
 const cors=require('cors')
 const app=express()
 app.use(cors())
 
-app.get('')
+app.get('/',cors(),(req,res)=>{
+    console.log(req);
+    res.send(JSON.stringify('hello mehrdad'))
+})
+app.listen(5000)
+
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 
 function App() {
-  
+  useEffect(()=>{
+    fetch('http://localhost:5000')
+    .then(res=>res.json())
+    .then(data=>console.log(data))
+  },[])
 
   return (
     <>
       <div>
-       
+        
       </div>
     </>
   )
