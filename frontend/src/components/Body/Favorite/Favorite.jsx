be1c78415c03e6f583fecae2f81c159f40fdd191
mehrdad <mehrdadosoulisaran@gmail.com>
2024-02-19 17:32:43 +0330

    set loop for favorite component

--- frontend/src/components/Body/Favorite/Favorite.jsx
+++ frontend/src/components/Body/Favorite/Favorite.jsx
@@ -1,12 +1,31 @@
-import React, { useEffect } from 'react'
-import styles from '../Favorite/favorite.module.css'
-export default function Favorite({data}) {
-  useEffect(()=>{
+import React, { useEffect } from "react";
+import styles from "../Favorite/favorite.module.css";
+export default function Favorite({ data }) {
+  useEffect(() => {
     console.log(data);
-  },[])
+  });
   return (
-    <div className={styles.container}>
-        <h2>Favorite</h2>
-    </div>
-  )
+    <>
+      <h2>Favorite</h2>
+      {data.map((item) => (
+        <div className={styles.left}>
+          <div>
+            <img src={`http://localhost:5000/assets/${item.image}`} alt="" />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.title}>
+              <h4>{item.title}</h4>
+            </div>
+            <div className={styles.title}>
+              <p>{item.author}</p>
+            </div>
+            <div className={styles.description}>
+              <div>{item.language }</div>
+              <div>{item.pages } pages</div>
+            </div>
+          </div>
+       </div>
+      ))}
+    </>
+  );
 }
