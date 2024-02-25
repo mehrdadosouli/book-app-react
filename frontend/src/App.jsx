d26290d65a6470fb2f02365011e0d328f5ebf57d
mehrdad Osouli Saran <mehrdadosoulisaran@gmail.com>
2024-02-25 20:23:45 +0330

    update code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
-// import { Provider } from "react-redux";
-// import store from "./app/store";
 
 function App() {
   return (
     <>
-      {/* <Provider store={store}> */}
         <div>
           <Header />
           <Body />
         </div>
-      {/* </Provider> */}
     </>
   );
 }
--- frontend/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import favoriteSlice from "./feauters/favoriteSlice";
-const store = configureStore({
-  reducer: {
-    favorite: favoriteSlice,
-  },
-});
-
-export default store;
